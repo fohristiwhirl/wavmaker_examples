52f58f4858ecb06eb35f4963c7a92b9f784567e8
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2016-07-24 16:42:31 +0100

    refactor

--- sine.go
+++ sine.go
@@ -5,18 +5,19 @@ import (
 	"math"
 )
 
-const FRAMES = 88200
-const FREQ = 440
 const TWO_PI = 6.28318530718
 
-func main() {
-	wav := wavmaker.NewWAV(FRAMES)
-
-	for n := uint32(0); n < FRAMES; n++ {
-		raw_val := 32767 * math.Sin((float64(n) / (wavmaker.PREFERRED_FREQ / TWO_PI)) * FREQ)
+func make_sine(freq float64, frames uint32) wavmaker.WAV {
+	wav := wavmaker.NewWAV(frames)
+	for n := uint32(0); n < frames; n++ {
+		raw_val := 32767 * math.Sin((float64(n) / (wavmaker.PREFERRED_FREQ / TWO_PI)) * freq)
 		val := int16(raw_val)
 		wav.Set(n, val, val)
 	}
+	return wav
+}
 
+func main() {
+	wav := make_sine(440, 88200)
 	wav.Save("sine.wav")
 }
