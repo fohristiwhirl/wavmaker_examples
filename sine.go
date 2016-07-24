a58d61af55873c82fb940fd9ef67587f7f8bfb17
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2016-07-24 17:56:00 +0100

    adjust to renamed function in library

--- sine.go
+++ sine.go
@@ -8,7 +8,7 @@ import (
 const TWO_PI = 6.28318530718
 
 func make_sine(freq float64, frames uint32) wavmaker.WAV {
-	wav := wavmaker.NewWAV(frames)
+	wav := wavmaker.New(frames)
 	for n := uint32(0); n < frames; n++ {
 		raw_val := 32767 * math.Sin((float64(n) / (wavmaker.PREFERRED_FREQ / TWO_PI)) * freq)
 		val := int16(raw_val)
